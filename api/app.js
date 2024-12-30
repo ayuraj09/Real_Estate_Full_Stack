5b66976346e0a6f1cc11b837e87ab33cdfc8bb89
Ayush Raj <105922280+ayuraj09@users.noreply.github.com>
2024-12-30 17:18:24 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -13,7 +13,10 @@ dotenv.config()
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'http://localhost:5173',
+              methods: ["GET", "POST", "PUT", "DELETE"],
+              allowedHeaders: ['Content-Type', 'Authorization'],
+              credentials: true }));
 // app.use(cors({ origin: 'https://realestatefrontend-nu.vercel.app/', credentials: true }));
 app.use(express.json())
 app.use(cookieParser())
