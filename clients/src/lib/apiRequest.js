64857cd478b83c69e446d49ecd3627d3ecd9ad8b
Ayush Raj <ayushr21@iitk.ac.in>
2025-01-12 23:02:05 +0530

    err

--- clients/src/lib/apiRequest.js
+++ clients/src/lib/apiRequest.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const env = 'prod';
 
 const apiRequest = axios.create({
-    baseURL: nv === "dev"
+    baseURL: env === "dev"
     ? "http://localhost:8800/api"
     : "https://realestatefrontend-nu.vercel.app/api",
     withCredentials:true
